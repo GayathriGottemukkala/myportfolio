5d999d6653a43347221983a8c0017c4419666077
GottemukkalaGayathri <gayathrigottemukkala123@gmail.com>
2024-11-16 12:39:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Experience from './Components/Experience/Experience';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Work from './Components/Work/Work';
+import Intro from './Components/Intro/Intro';
+import Navbar from './Components/Navbar/Navbar';
+import Services from './Components/Services/Services';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import { useContext, useEffect } from "react";
+import { themeContext } from "./Context.jsx";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  console.log(darkMode,"darkMode")
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}>
+        <div>working</div>
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Work />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
